fb96a9345f287dc21c0bd09b53f569a8557a70c4
Maria Jaworska <mkjawors@wp.pl>
2023-12-22 15:09:15 +0100

    new commit

--- src/Calculator.java
+++ src/Calculator.java
@@ -1,4 +1,90 @@
-public class Calculator {
-    public static void main(String[] args) {
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+
+public class Calculator implements ActionListener {
+
+    JFrame frame;
+    JTextField textField;
+    JButton[] numberButtons = new JButton[10];
+    JButton[] functionButtons = new JButton[8];
+    JButton addButton, subButton, mulButton, divButton;
+    JButton delButton, eqButton, decButton, clrButton;
+    JPanel panel;
+
+    Font myFont = new Font("Serif", Font.BOLD, 30);
+
+    double num1 = 0, num2 = 0, result = 0;
+    char operator;
+
+    Calculator() {
+
+        frame = new JFrame("Calculator");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(420, 550);
+        frame.setLayout(null);
+
+
+        textField = new JTextField();
+        textField.setBounds(50, 25, 300, 50);
+        textField.setFont(myFont);
+        textField.setEditable(false);
+
+        addButton = new JButton("+");
+        subButton = new JButton("-");
+        mulButton = new JButton("*");
+        divButton = new JButton("/");
+        decButton = new JButton(".");
+        eqButton = new JButton("=");
+        delButton = new JButton("delete");
+        clrButton = new JButton("clear");
+
+        functionButtons[0] = addButton;
+        functionButtons[1] = subButton;
+        functionButtons[2] = mulButton;
+        functionButtons[3] = divButton;
+        functionButtons[4] = delButton;
+        functionButtons[5] = eqButton;
+        functionButtons[6] = delButton;
+        functionButtons[7] = clrButton;
+
+        for(int i = 0; i < 8; i++) {
+            functionButtons[i].addActionListener(this);
+            functionButtons[i].setFont(myFont);
+            functionButtons[i].setFocusable(false);
+        }
+
+        for(int i = 0; i < 10; i++) {
+            numberButtons[i] = new JButton(String.valueOf(i));
+            numberButtons[i].addActionListener(this);
+            numberButtons[i].setFont(myFont);
+            numberButtons[i].setFocusable(false);
+        }
+
+        delButton.setBounds(50,430, 145, 50);
+        clrButton.setBounds(205,430, 145, 50);
+
+        panel = new JPanel();
+        panel.setBounds(50, 100, 300 , 300);
+        panel.setLayout(new GridLayout(4, 4, 10, 10));
+        panel.setBackground(Color.GRAY);
+
+
+        frame.add(panel);
+        frame.add(delButton);
+        frame.add(clrButton);
+        frame.add(textField);
+        frame.setVisible(true);
+
     }
+    public static void main(String[] args) {
+
+        Calculator calc = new Calculator();
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
+    }
+
 }
